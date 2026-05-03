57fb1ca6628fc27b7d44f70b8fb70355d5c9559a
zhangxc365-source <zhang.x.c365@gmail.com>
2026-05-03 15:33:27 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: '/Triple-Match-up/',
+    base: '/Triple-match-up/',
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
